f5db3e71120d4cbf6ca853bce67706ffd0e07cf6
Xin Wang <sylvia.wang@berkeley.edu>
2019-10-03 15:33:33 -0700

    Add header and load packages

---
+++ code/demo-script.R
@@ -0,0 +1,16 @@
+# ===================================================================
+# Title: STAT133-lab05
+# Description:
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Xin Wang
+# Date: 10-03-2019
+# ===================================================================
+
+
+# packages
+library(dplyr)    # data wrangling
+library(ggplot2)  # graphics
+
